327bff713c9bf9cf3df40c7c056543964fe31901
HajunAndroid <didtmdgus100@gmail.com>
2021-09-06 21:27:41 +0900

    Retrofit2 News

---
+++ News/src/main/java/kr/co/hajun/retrofit2/ItemModel.java
@@ -0,0 +1,10 @@
+package kr.co.hajun.retrofit2;
+
+public class ItemModel {
+    public long id;
+    public String author;
+    public String title;
+    public String description;
+    public String urlToImage;
+    public String publishedAt;
+}
